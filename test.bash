80233567326167ece7fed3cf505424da19a19dd1
Akito Hamano <tsukigamichibikuisekaidochu@gmail.com>
2024-11-30 14:50:44 +0900

    correct code

--- test.bash
+++ test.bash
@@ -2,6 +2,13 @@
 # SPDX-FileCopyrightText: 2024 Akito Hamano
 # SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+    echo "${1}行目がちゃうよ"
+    res=1
+}
+
+res=0
+
 # 正常な入力
 output="xの平均:2.0   yの平均:5.0   x*yの平均:10.67   xの二乗の平均:4.67   xの平均の二乗:4.0最小二乗法の式は y = 1.0x + 3.0"
 cleaned_output=$(echo "$output" | tr -d '[:space:]')
@@ -12,30 +19,27 @@ cleaned_out=$(echo "$out" | tr -d '[:space:]')
 
 # 異常な入力
 out=$(echo -e あ\\nい | ./OLS)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" != "" ] || ng "$LINENO"
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo -e  | ./OLS)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" != "" ] || ng "$LINENO"
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo -e 1 2\\n3 4 5 | ./OLS)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" != "" ] || ng "$LINENO"
 
 out=$(echo -e 1 2 3\\n4 5 | ./OLS)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" != "" ] || ng "$LINENO"
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo -e 1 2 あ\\n3 4 5 | ./OLS)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" != "" ] || ng "$LINENO"
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo -e 1 2 a\\n3 4 5 | ./OLS)
-[ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" != "" ] || ng "$LINENO"
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 
-[ "${res}" = 0 ] && echo OK
+[ "$res" = 0 ] && echo OK
 exit $res
 
